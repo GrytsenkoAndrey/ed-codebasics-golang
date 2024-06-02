310b9d6d8ed2a58caf2daf3f92a50ddc6bb32e70
Andrey Grytsenko <andreyapg@gmail.com>
2024-06-02 11:17:34 +0300

    10 alternative conditions 2024-06-02

---
+++ part_1/10_alternative_conditions.go
@@ -0,0 +1,28 @@
+// x := 10
+
+// switch x {
+//     default: // default всегда выполняется последним независимо от расположения в конструкции
+//         fmt.Println("default case")
+//     case 10:
+//         fmt.Println("case 10")
+// }
+
+// Однако при необходимости можно реализовать логику С-подобных языков и «провалиться» в следующий case:
+
+// x := 10
+
+// switch { // выражение отсутствует. Для компилятора выглядит как: switch true
+// 	default:
+// 		fmt.Println("default case")
+// 	case x == 10:
+// 		fmt.Println("equal 10 case")
+// 		fallthrough
+// 	case x <= 10:
+// 		fmt.Println("less or equal 10 case")
+// }
+
+package solution
+
+import (
+	"strings"
+)
