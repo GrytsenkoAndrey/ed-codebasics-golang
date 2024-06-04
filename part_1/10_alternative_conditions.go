b421610e4146e6e3c977cb2ef29fda2064be8bc1
Andrey Grytsenko <andreyapg@gmail.com>
2024-06-04 08:26:13 +0300

    10 Theory 2024-06-04

--- part_1/10_alternative_conditions.go
+++ part_1/10_alternative_conditions.go
@@ -27,6 +27,47 @@
 // strings.ReplaceAll("one two three", " ", "_") // one_two_three
 
 
+// Go: Альтернативная условная конструкция
+
+// В Go присутствует единственная альтернатива if — конструкция switch. Для этой конструкции используется стандартный синтаксис, но логика работы отличается от С-подобных языков. Когда срабатывает условие какого-либо case, программа выполняет блок и выходит из конструкции switch без необходимости писать break:
+
+// x := 10
+
+// switch x {
+//     default: // default всегда выполняется последним независимо от расположения в конструкции
+//         fmt.Println("default case")
+//     case 10:
+//         fmt.Println("case 10")
+// }
+
+// Output:
+
+// case 10
+
+// Однако при необходимости можно реализовать логику С-подобных языков и «провалиться» в следующий case:
+
+// x := 10
+
+// switch { // выражение отсутствует. Для компилятора выглядит как: switch true
+//     default:
+//         fmt.Println("default case")
+//     case x == 10:
+//         fmt.Println("equal 10 case")
+//         fallthrough
+//     case x <= 10:
+//         fmt.Println("less or equal 10 case")
+// }
+
+// Output:
+
+// equal 10 case
+// less or equal 10 case
+
+// Задание
+
+// Реализуйте функцию ModifySpaces(s, mode string) string, которая изменяет строку s в зависимости от переданного режима mode.
+
+
 package solution
 
 import (
