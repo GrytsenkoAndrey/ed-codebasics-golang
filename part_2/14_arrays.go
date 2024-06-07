dd4943aee05b2b159b14a224808e7b9c888aa572
Andrey Grytsenko <andreyapg@gmail.com>
2024-06-07 11:31:05 +0300

    14 Arrays 2024-06-07

--- part_2/14_arrays.go
+++ part_2/14_arrays.go
@@ -60,3 +60,13 @@
 // Задание
 
 // Реализуйте функцию SafeWrite(nums [5]int, i, val int) [5]int, которая записывает значение val в массив nums по индексу i, если индекс находится в рамках массива. В противном случае массив возвращается без изменения.
+
+func SafeWrite(nums [5]int, i, val int) [5]int {
+	if i > 4 || i < 0 {
+		return nums
+	}
+
+	nums[i] = val
+
+	return nums
+}
